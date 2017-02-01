bdea2fe0aa07ba49c0ac26ba2dd7d492d019093b
Suppoze <tegyukfel@gmail.com>
2017-02-01 19:34:43 +0100

    Added RxJava, changed random Giphy request

--- src/main/kotlin/giphy/GiphyCommand.kt
+++ src/main/kotlin/giphy/GiphyCommand.kt
@@ -5,15 +5,16 @@ import sx.blah.discord.handle.impl.events.MessageReceivedEvent
 
 class GiphyCommand(val event: MessageReceivedEvent, val tag: String) : Command {
 
-    private val onSuccess: (String) -> Unit = {
+    private val onNext: (String) -> Unit = {
         event.message.reply(it)
     }
 
-    private val onFailure: () -> Unit = {
-        event.message.channel.sendMessage("Error during giphy request, bork :(")
+    private val  onError: ((t: Throwable) -> Unit)? = {
+        event.message.channel.sendMessage("Error during giphy request!")
     }
 
     override fun perform() {
-        GiphyServer().requestRandomGiphyUrlByTag(tag, onSuccess, onFailure)
+        GiphyServer().requestRandomGiphyUrlByTag(tag)
+                .subscribe(onNext, onError)
     }
 }
--- src/main/kotlin/giphy/GiphyServer.kt
+++ src/main/kotlin/giphy/GiphyServer.kt
@@ -1,26 +1,16 @@
 package giphy
 
 import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
+import com.github.kittinunf.fuel.rx.rx_object
+import rx.Observable
 
 class GiphyServer {
 
-    fun requestRandomGiphyUrlByTag(tag: String,
-                                   onSuccess: (String) -> Unit,
-                                   onFailure: () -> Unit) {
-        "http://api.giphy.com/v1/gifs/random"
+    fun requestRandomGiphyUrlByTag(tag: String): Observable<String> {
+        return "http://api.giphy.com/v1/gifs/random"
                 .httpGet(listOf("api_key" to "dc6zaTOxFJmzC", "tag" to tag, "rating" to "r"))
-                .responseObject(GiphyRandomResponse.Deserializer()) { request, response, result ->
-                    when (result) {
-                        is Result.Failure -> {
-                            println(result.error.localizedMessage)
-                            onFailure()
-                        }
-                        is Result.Success -> {
-                            onSuccess(result.value.data.image_original_url)
-                        }
-                    }
-                }
+                .rx_object(GiphyRandomResponse.Deserializer())
+                .retry(1)
+                .map { it.data.url }
     }
-
 }
