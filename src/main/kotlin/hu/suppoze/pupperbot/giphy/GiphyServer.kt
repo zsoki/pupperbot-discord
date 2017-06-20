92419e9736717c024a7fa52269769a4e7bbdc338
Suppoze <tegyukfel@gmail.com>
2017-06-20 23:14:59 +0200

    Updated libraries, Kotlin, fixed giphy server request observable

--- src/main/kotlin/hu/suppoze/pupperbot/giphy/GiphyServer.kt
+++ src/main/kotlin/hu/suppoze/pupperbot/giphy/GiphyServer.kt
@@ -4,19 +4,19 @@ import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.rx.rx_object
 import com.github.salomonbrys.kodein.instance
 import hu.suppoze.pupperbot.di.kodein
-import rx.Observable
+import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 
 class GiphyServer {
 
     private val giphyUrl: String by kodein.instance("giphyUrl")
 
-    fun requestRandomGiphyUrlByTag(tag: String): Observable<String> {
+    fun requestRandomGiphyUrlByTag(tag: String): Single<String> {
         return giphyUrl
                 .httpGet(listOf("api_key" to "dc6zaTOxFJmzC", "tag" to tag, "rating" to "r")) // The API key is public demo key, don't panic
                 .rx_object(GiphyRandomResponse.Deserializer())
                 .timeout(5, TimeUnit.SECONDS)
                 .retry(1)
-                .map { it.data.url }
+                .map { it.get().data.url }
     }
 }
